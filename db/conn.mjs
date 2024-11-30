91b32d0aabbb85f54dd010d509efd4d84483942d
Sinchana-Rai <sinchana.srai@gmail.com>
2024-11-30 10:23:03 -0500

    refactoring done for add and delete

--- db/conn.mjs
+++ db/conn.mjs
@@ -1,17 +1,16 @@
-import { MongoClient } from "mongodb";
-
+import mongoose from "mongoose";
 import dotenv from "dotenv";
+
 dotenv.config();
 
-const client = new MongoClient(process.env.ATLAS_URI);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true });
+    console.log("Connected to DB");
+  } catch (error) {
+    console.log('connection error', error);
+    process.exit(1);
+  }
+};
 
-let conn;
-try {
-  conn = await client.connect();
-} catch (e) {
-  console.error(e);
-}
-
-let db = conn.db("sample_training");
-
-export default db;
+export default connectDB;
--- index.mjs
+++ index.mjs
@@ -1,34 +1,40 @@
 import express from "express";
 
-import mongoose from "mongoose";
+import connectDB from "./db/conn.mjs";
 
-const PORT = process.env.PORT || 5050;
-const app = express();
-
-//imported gradesschema
-import Grade from "./routes/grades.mjs";
-
-// import grades from "./routes/grades.mjs";
+import grades from "./routes/grades.mjs";
 import grades_agg from "./routes/grades_agg.mjs";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5050;
+
+// Connect to Mongoose
+connectDB();
 
 app.use(express.json());
 
-// Connect to Mongoose.
-await mongoose.connect(process.env.ATLAS_URI);
+// Routes
+
+
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the API.");
-});
+    res.send("Welcome to the API.");
+  });
 
-app.use("/grades", Grade);
-app.use("/grades", grades_agg);
+
+  app.use("/grades", grades);
+  app.use("/grades_agg", grades_agg);
 
 // Global error handling
 app.use((err, _req, res, next) => {
-  res.status(500).send("Seems like we messed up somewhere...");
-});
-
-// Start the Express server
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
-});
+    res.status(500).send("Seems like we messed up somewhere...");
+  });
+  
+  // Start the Express server
+  app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+  });
+  
